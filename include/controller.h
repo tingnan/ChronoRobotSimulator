280811fc95a5a6b25d7428081415666798c8c8d6
tingnan <tingnan.zhang@gmail.com>
2015-10-26 11:33:58 -0400

    snake update

--- include/controller.h
+++ include/controller.h
@@ -43,36 +43,4 @@ private:
 
 void UseController(Controller *controller);
 void UsePositionControl(Robot *robot);
-
-namespace chrono {
-class ChFunctionController : public ChFunction {
-public:
-  ChFunctionController(size_t index, Controller *controller)
-      : index_(index), controller_(controller) {}
-  ~ChFunctionController() {}
-  ChFunction *new_Duplicate() {
-    return new ChFunctionController(index_, controller_);
-  }
-  int Get_Type() { return 9527; }
-  double Get_y(double curr_t);
-  double Get_y_dx(double new_t) { return 0; }
-
-  double p_gain = 0.70;
-  double i_gain = 0.00;
-  double d_gain = 1e-3;
-  double torque_limit = 1.0;
-  double angle_limit = 1.2;
-
-protected:
-  double GetMediaTorque(double t);
-  double GetContactTorque(double t);
-  // The low level PID controller in motor.
-  double ComputeDriveTorque(double t);
-  double ComputeLimitTorque(double t);
-  double cum_error_ = 0;
-  Controller *controller_;
-  size_t index_;
-};
-} // namespace chrono
-
 #endif // INCLUDE_CHFUNCTION_CONTROLLER_H_
--- src/controller.cpp
+++ src/controller.cpp
@@ -1,6 +1,7 @@
 #include <Eigen/Dense>
 
 #include "include/controller.h"
+#include "include/chfunction_controller.h"
 #include "include/robot.h"
 #include "include/vector_utility.h"
 
@@ -394,58 +395,6 @@ double Controller::GetAngularSpeed(size_t index, double t) {
   double desired_angular_speed =
       default_amplitude_ * omega_ * cos(omega_ * t + phase);
   return desired_angular_speed;
-}
-
-// The parameters for the function
-double ChFunctionController::Get_y(double t) {
-  double torque =
-      ComputeDriveTorque(t) + ComputeLimitTorque(t) + 1.0 * GetMediaTorque(t);
-  /*
-double torque_contact = GetContactTorque(t);
-double desired_angular_speed = controller_->GetAngularSpeed(index_, t);
-if (torque_contact * desired_angular_speed > 0) {
-torque += 0.0 * torque_contact;
-}
-  */
-  // torque += 1.0 * torque_contact;
-
-  torque = std::max(std::min(torque_limit, torque), -torque_limit);
-  return torque;
-}
-
-double ChFunctionController::GetContactTorque(double t) {
-  return controller_->GetContactTorque(index_, t);
-}
-double ChFunctionController::GetMediaTorque(double t) {
-  return controller_->GetMediaTorque(index_, t);
-}
-
-// The low level PID controller in motor.
-double ChFunctionController::ComputeDriveTorque(double t) {
-  double desired_angle = controller_->GetAngle(index_, t);
-  double desired_angular_speed = controller_->GetAngularSpeed(index_, t);
-  double curr_angle = controller_->GetEngine(index_)->Get_mot_rot();
-  double curr_angular_speed = controller_->GetEngine(index_)->Get_mot_rot_dt();
-  // std::cout << index_ << " " << curr_angular_speed << std::endl;
-  cum_error_ += desired_angle - curr_angle;
-  double torque = p_gain * (desired_angle - curr_angle) +
-                  d_gain * (desired_angular_speed - curr_angular_speed) +
-                  cum_error_ * i_gain;
-  // torque -= 1e-3 * curr_angular_speed;
-  return torque;
-}
-
-double ChFunctionController::ComputeLimitTorque(double t) {
-  auto engine = controller_->GetEngine(index_);
-  double curr_angle = engine->Get_mot_rot();
-  double angle_limit_compliance = 1;
-  if (curr_angle > angle_limit) {
-    return -angle_limit_compliance * (curr_angle - angle_limit);
-  }
-  if (curr_angle < -angle_limit) {
-    return -angle_limit_compliance * (curr_angle - angle_limit);
-  }
-  return 0;
 }
 
 void UsePositionControl(Robot *robot) {
