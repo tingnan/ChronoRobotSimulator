27a4bd7124d4b62e099430ddab0b09ff824506f2
tingnan <tingnan.zhang@gmail.com>
2016-01-29 16:25:22 -0500

    structure change

---
+++ include/servo_motor.h
@@ -0,0 +1,52 @@
+#ifndef INCLUDE_CHFUNCTION_CONTROLLER_H_
+#define INCLUDE_CHFUNCTION_CONTROLLER_H_
+
+#include <cmath>
+#include <algorithm>
+#include <physics/ChLinkEngine.h>
+#include <motion_functions/ChFunction_Base.h>
+
+class Controller;
+
+namespace chrono {
+
+class ChFunctionPID : public ChFunction {
+public:
+  ChFunctionPID(ChSharedPtr<ChFunction> motor_funct,
+                ChSharedPtr<ChLinkEngine> engine)
+      : motor_funct_(motor_funct), engine_(engine) {}
+  ~ChFunctionPID() {}
+  ChFunction *new_Duplicate() {
+    return new ChFunctionPID(motor_funct_, engine_);
+  }
+  int Get_Type() { return 9527; }
+  double Get_y(double curr_t);
+  double Get_y_dx(double curr_t) { return 0; }
+
+protected:
+  double p_gain_ = 3.40;
+  double i_gain_ = 0.00;
+  double d_gain_ = 1e-3;
+  double t_limit_ = 1.5;
+  double a_limit_ = 1.2;
+
+  double cum_error_ = 0;
+  ChSharedPtr<ChFunction> motor_funct_;
+  ChSharedPtr<ChLinkEngine> engine_;
+};
+
+class ChServoMotor {
+public:
+  ChServoMotor(ChSharedPtr<ChLinkEngine> engine,
+               ChSharedPtr<ChFunction> motor_funct,
+               ChLinkEngine::eCh_eng_mode mode_flag);
+  void Serialize();
+
+private:
+  // The ChEngine underying
+  ChSharedPtr<ChLinkEngine> engine_;
+};
+
+} // namespace chrono
+
+#endif // INCLUDE_CHFUNCTION_CONTROLLER_H_
---
+++ src/servo_motor.cpp
@@ -0,0 +1,30 @@
+#include "include/servo_motor.h"
+
+using namespace chrono;
+
+double ChFunctionPID::Get_y(double t) {
+  double desired_angle = motor_funct_->Get_y(t);
+  double desired_angular_speed = motor_funct_->Get_y_dx(t);
+  double curr_angle = engine_->Get_mot_rot();
+  double curr_angular_speed = engine_->Get_mot_rot_dt();
+  cum_error_ += desired_angle - curr_angle;
+  double torque = p_gain_ * (desired_angle - curr_angle) +
+                  d_gain_ * (desired_angular_speed - curr_angular_speed) +
+                  cum_error_ * i_gain_;
+  torque = std::max(std::min(t_limit_, torque), -t_limit_);
+  return torque;
+}
+
+ChServoMotor::ChServoMotor(ChSharedPtr<ChLinkEngine> engine,
+                           ChSharedPtr<ChFunction> motor_funct,
+                           ChLinkEngine::eCh_eng_mode mode_flag)
+    : engine_(engine) {
+  engine_->Set_eng_mode(mode_flag);
+  if (mode_flag == ChLinkEngine::ENG_MODE_ROTATION) {
+    engine_->Set_rot_funct(motor_funct);
+  } else if (mode_flag == ChLinkEngine::ENG_MODE_TORQUE) {
+    ChSharedPtr<ChFunctionPID> torque_funct(
+        new ChFunctionPID(motor_funct, engine));
+    engine_->Set_tor_funct(motor_funct);
+  }
+}
